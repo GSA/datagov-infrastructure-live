5e325f1844ef13ade19b343aba4d9d0850f2c30a
Dan Olaru <dan.olaru@reisystems.com>
2017-08-28 17:51:28 -0400

    adjust number of subnets

--- vpc/main.tf
+++ vpc/main.tf
@@ -4,10 +4,12 @@ module "vpc" {
   name = "datagov-terragrunt"
 
   cidr = "10.0.0.0/16"
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  private_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets   = ["10.0.101.0/24", "10.0.102.0/24"]
+  database_subnets = ["10.0.201.0/24", "10.0.202.0/24"]
 
   enable_nat_gateway = "true"
+  single_nat_gateway = "true"
 
   azs      = ["us-east-1a", "us-east-1b"]
 
