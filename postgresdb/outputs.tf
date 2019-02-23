54a71cae981904c891fe5734a90114a7dd6221a6
Aaron D Borden <adborden@a14n.net>
2019-02-23 11:54:36 -0800

    Add postgresdb module
    A standard way to create postgresql databases

---
+++ postgresdb/outputs.tf
@@ -0,0 +1,16 @@
+output "db_name" {
+  value = "${module.database.db_name}"
+}
+
+output "db_username" {
+  value = "${module.database.db_username}"
+}
+
+output "db_password" {
+  value     = "${module.database.db_password}"
+  sensitive = true
+}
+
+output "db_server" {
+  value = "${module.database.db_server}"
+}
---
+++ postgresdb/variables.tf
@@ -0,0 +1,31 @@
+variable "aws_region" {
+  description = "The AWS region to create instances."
+}
+
+variable "env" {
+  description = "Name of the environment."
+}
+
+variable "db_instance_class" {
+  default = "db.t2.micro"
+}
+
+variable "db_name" {
+  description = "Name of the database to create."
+}
+
+variable "db_username" {
+  description = "User to create for the database."
+  default = "dbuser"
+}
+
+variable "db_password" {
+  description = "Password to set for the database user."
+}
+
+variable "db_skip_final_snapshot" {
+  description = "Create a final snapshot on destroy?"
+  default  = true
+}
+
+
