7bc3e1803fee0530a704949ae46b88cd8e6cd02f
Aaron D Borden <adborden@a14n.net>
2019-03-28 23:10:20 -0700

    More outputs

--- inventory/outputs.tf
+++ inventory/outputs.tf
@@ -2,10 +2,6 @@ output "db_name" {
   value = "${module.db.db_name}"
 }
 
-output "db_username" {
-  value = "${module.db.db_username}"
-}
-
 output "db_password" {
   value     = "${module.db.db_password}"
   sensitive = true
@@ -13,6 +9,10 @@ output "db_password" {
 
 output "db_server" {
   value = "${module.db.db_server}"
+}
+
+output "db_username" {
+  value = "${module.db.db_username}"
 }
 
 output "dns" {
