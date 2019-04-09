274699d03ebbf821e8ece1af43d557d9e4febbe2
Aaron D Borden <adborden@a14n.net>
2019-04-09 16:47:09 -0700

    More missing dependencies for python build
    https://github.com/pyenv/pyenv/wiki#suggested-build-environment

--- jumpbox/bin/provision.sh
+++ jumpbox/bin/provision.sh
@@ -9,7 +9,22 @@ sudo apt update
 # Dependencies to run ansible
 sudo apt install -y \
   build-essential \
+  curl \
   git \
+  libbz2-dev \
+  libffi-dev \
+  liblzma-dev \
+  libncurses5-dev \
+  libreadline-dev \
+  libsqlite3-dev \
+  libssl-dev \
+  libxml2-dev \
+  libxmlsec1-dev \
+  llvm \
+  python3-pip \
   python-dev \
   python-virtualenv \
+  tk-dev \
+  wget \
+  xz-utils \
   zlib1g-dev
