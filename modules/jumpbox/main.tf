41d51122ea194984bc464cbc60cc85a0e09b695a
Aaron D Borden <adborden@a14n.net>
2019-09-12 13:41:41 -0700

    Ignore AMI updates on jumpbox
    Avoid destroy/recreate for new AMI images.

--- modules/jumpbox/main.tf
+++ modules/jumpbox/main.tf
@@ -131,6 +131,12 @@ resource "aws_instance" "jumpbox" {
     user = "ubuntu"
   }
 
+  lifecycle {
+    ignore_changes = [
+      "ami",
+    ]
+  }
+
   provisioner "remote-exec" {
     script = "${path.module}/bin/provision.sh"
   }
