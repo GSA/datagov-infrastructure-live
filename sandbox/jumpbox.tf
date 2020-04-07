a82b8796f558bd7f6ee85d1354d4591061f2b80a
Aaron D Borden <adborden@a14n.net>
2020-04-06 17:01:52 -0700

    [jumpbox] add jumpbox to sandbox

---
+++ sandbox/jumpbox.tf
@@ -0,0 +1,11 @@
+module "jumpbox" {
+  source = "github.com/gsa/datagov-infrastructure-modules.git//modules/jumpbox?ref=feature-terraform-12"
+
+  ami_filter_name  = var.ami_filter_name
+  dns_zone_public  = module.vpc.dns_zone_public
+  dns_zone_private = module.vpc.dns_zone_private
+  env              = var.env
+  key_name         = var.key_name
+  public_subnets   = module.vpc.public_subnets
+  vpc_id           = module.vpc.vpc_id
+}
