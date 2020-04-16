f9a00d116238c05708ed79110eb1b99294ecfec9
Aaron D Borden <adborden@a14n.net>
2020-04-16 16:11:38 -0700

    [sandbox] output jumpbox dns

--- sandbox/outputs.tf
+++ sandbox/outputs.tf
@@ -18,6 +18,10 @@ output "inventory_2_8_db_password" {
   value     = module.inventory_2_8.db_password
 }
 
+output "jumpbox_dns" {
+  value     = module.jumpbox.jumpbox_dns
+}
+
 output "wordpress_db_password" {
   sensitive = true
   value     = module.wordpress.db_password
