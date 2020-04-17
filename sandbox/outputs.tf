18e83acf8801d4cd10d442d568cb3ce50b86f6c3
Aaron D Borden <adborden@a14n.net>
2020-04-16 17:02:15 -0700

    Outputs for db hosts

--- sandbox/outputs.tf
+++ sandbox/outputs.tf
@@ -3,9 +3,19 @@ output "catalog_db_password" {
   value     = module.catalog.db_password
 }
 
+output "catalog_db_host" {
+  sensitive = true
+  value     = module.catalog.db_server
+}
+
 output "dashboard_db_password" {
   sensitive = true
   value     = module.dashboard.db_password
+}
+
+output "dashboard_db_host" {
+  sensitive = true
+  value     = module.dashboard.db_server
 }
 
 output "inventory_db_password" {
@@ -13,9 +23,19 @@ output "inventory_db_password" {
   value     = module.inventory.db_password
 }
 
+output "inventory_db_host" {
+  sensitive = true
+  value     = module.inventory.db_server
+}
+
 output "inventory_2_8_db_password" {
   sensitive = true
   value     = module.inventory_2_8.db_password
+}
+
+output "inventory_2_8_db_host" {
+  sensitive = true
+  value     = module.inventory_2_8.db_server
 }
 
 output "jumpbox_dns" {
@@ -25,4 +45,9 @@ output "jumpbox_dns" {
 output "wordpress_db_password" {
   sensitive = true
   value     = module.wordpress.db_password
+}
+
+output "wordpress_db_host" {
+  sensitive = true
+  value     = module.wordpress.db_server
 }
