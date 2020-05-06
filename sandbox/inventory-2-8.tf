fd36ccb6f9f6f002d585fdf5def0493ff2311073
Aaron D Borden <adborden@a14n.net>
2020-05-06 08:34:48 -0700

    [inventory] enable redis for 2.8+ (#65)

--- sandbox/inventory-2-8.tf
+++ sandbox/inventory-2-8.tf
@@ -1,5 +1,5 @@
 module "inventory_2_8" {
-  source = "github.com/gsa/datagov-infrastructure-modules.git//modules/inventory?ref=v3.0.0"
+  source = "github.com/gsa/datagov-infrastructure-modules.git//modules/inventory?ref=v3.2.0"
 
   ansible_group         = "inventory_web,inventory_web_2_8,v2"
   bastion_host          = module.jumpbox.jumpbox_dns
@@ -8,6 +8,7 @@ module "inventory_2_8" {
   db_password           = var.inventory_2_8_db_password
   dns_zone_private      = module.vpc.dns_zone_private
   dns_zone_public       = module.vpc.dns_zone_public
+  enable_redis          = true
   env                   = var.env
   key_name              = var.key_name
   subnets_private       = module.vpc.private_subnets
