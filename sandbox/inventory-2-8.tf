ff4e54dbb970df2a4e7c3a8730228305e8bb35b2
Aaron D Borden <adborden@a14n.net>
2020-05-07 21:57:05 -0700

    Fixes for catalog/redis (#69)
    * [sandbox] outputs for redis
    
    * [catalog] fix harvester and redis
    
    * [catalog] specify pycsw and v1 ansible groups
    
    * [inventory] fix typo in redis security groups
    
    * [inventory] add s3 bucket name
    
    Required variable.

--- sandbox/inventory-2-8.tf
+++ sandbox/inventory-2-8.tf
@@ -1,5 +1,5 @@
 module "inventory_2_8" {
-  source = "github.com/gsa/datagov-infrastructure-modules.git//modules/inventory?ref=v3.2.0"
+  source = "github.com/gsa/datagov-infrastructure-modules.git//modules/inventory?ref=v3.4.1"
 
   ansible_group         = "inventory_web,inventory_web_2_8,v2"
   bastion_host          = module.jumpbox.jumpbox_dns
@@ -11,6 +11,7 @@ module "inventory_2_8" {
   enable_redis          = true
   env                   = var.env
   key_name              = var.key_name
+  s3_bucket_name        = "datagov-appdata-inventory-2-8-${var.env}"
   subnets_private       = module.vpc.private_subnets
   subnets_public        = module.vpc.public_subnets
   web_instance_name     = "inventory-2-8"
