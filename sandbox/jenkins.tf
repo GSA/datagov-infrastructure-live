ee93e97a4d5e227f8108eb344661968cf8212b1f
Fuhu Xia <fxia@reisystems.com>
2020-06-19 15:16:22 -0400

    update for adding lb to jenkins, use tag v4.0.0

--- sandbox/jenkins.tf
+++ sandbox/jenkins.tf
@@ -1,5 +1,5 @@
 module "jenkins" {
-  source = "github.com/gsa/datagov-infrastructure-modules.git//modules/jenkins?ref=v3.8.0"
+  source = "github.com/gsa/datagov-infrastructure-modules.git//modules/jenkins?ref=v4.0.0"
 
   availability_zones        = module.vpc.azs
   bastion_host              = module.jumpbox.jumpbox_dns
