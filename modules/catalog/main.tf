3be1c9894d5238297eb37220d9d5a0a0c51b1773
Fuhu Xia <fxia@reisystems.com>
2020-07-10 10:51:28 -0400

    no need for port 80 for fgdc2iso

--- modules/catalog/main.tf
+++ modules/catalog/main.tf
@@ -159,13 +159,6 @@ resource "aws_security_group" "fgdc2iso" {
   vpc_id      = var.vpc_id
 
   ingress {
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    security_groups = [aws_security_group.harvester.id]
-  }
-
-  ingress {
     from_port       = 443
     to_port         = 443
     protocol        = "tcp"
