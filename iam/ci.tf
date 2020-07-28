dfb35b8f24ffa3df6f1c91846c9c43b865317645
jbrown-xentity <jbrown@xentity.com>
2020-07-28 07:06:02 -0600

    Add ci policy

--- iam/ci.tf
+++ iam/ci.tf
@@ -13,3 +13,23 @@ resource "aws_iam_policy" "ci" {
 
   policy = file("files/aws_iam_policy_ci_terraform.json")
 }
+
+resource "aws_iam_user_policy" "ci_policy" {
+  name       = "ci_policy"
+  group      = aws_iam_user.ci.name
+  policy     = <<EOF
+{
+  "Version": "2020-07-27",
+  "Statement": [
+    {
+      "Action": [
+        "iam:DeleteRolePolicy",
+        "iam:RemoveRoleFromInstanceProfile"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+} add to ci user
+EOF
+}
