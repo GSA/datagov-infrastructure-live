43914bfa152c8db368cb846c904260864e23baca
Aaron D Borden <adborden@a14n.net>
2020-12-11 06:32:12 -0800

    [jenkins] create new instance for saml testing (#121)

--- sandbox/jenkins.tf
+++ sandbox/jenkins.tf
@@ -1,5 +1,5 @@
 module "jenkins" {
-  source = "github.com/gsa/datagov-infrastructure-modules.git//modules/jenkins?ref=v4.0.2"
+  source = "github.com/gsa/datagov-infrastructure-modules.git//modules/jenkins?ref=v4.2.0"
 
   availability_zones        = module.vpc.azs
   bastion_host              = module.jumpbox.jumpbox_dns
@@ -9,6 +9,28 @@ module "jenkins" {
   ebs_size                  = 60
   env                       = var.env
   key_name                  = var.key_name
+  subnets_private           = module.vpc.private_subnets
+  subnets_public            = module.vpc.public_subnets
+  vpc_id                    = module.vpc.vpc_id
+
+  security_groups = [
+    module.vpc.security_group_id,
+  ]
+}
+
+module "jenkins_saml" {
+  source = "github.com/gsa/datagov-infrastructure-modules.git//modules/jenkins?ref=v4.2.0"
+
+  availability_zones        = module.vpc.azs
+  bastion_host              = module.jumpbox.jumpbox_dns
+  default_security_group_id = module.vpc.security_group_id
+  dns_zone_private          = module.vpc.dns_zone_private
+  dns_zone_public           = module.vpc.dns_zone_public
+  ebs_size                  = 8
+  env                       = var.env
+  key_name                  = var.key_name
+  instance_name_format      = "jenkins-saml%dtf"
+  name                      = "jenkins-saml"
   subnets_private           = module.vpc.private_subnets
   subnets_public            = module.vpc.public_subnets
   vpc_id                    = module.vpc.vpc_id
