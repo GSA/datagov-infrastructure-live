c2fb00fc80722a995a8f4bddd70efc5b8293c535
Fuhu Xia <fxia@reisystems.com>
2020-12-15 16:17:16 -0500

    update ckan modules to use https in LB (#125)

--- sandbox/inventory-next.tf
+++ sandbox/inventory-next.tf
@@ -1,5 +1,5 @@
 module "inventory_next" {
-  source = "github.com/gsa/datagov-infrastructure-modules.git//modules/inventory?ref=v4.2.0"
+  source = "github.com/gsa/datagov-infrastructure-modules.git//modules/inventory?ref=v4.2.1"
 
   ansible_group         = "inventory_web,inventory_web_next,v2"
   bastion_host          = module.jumpbox.jumpbox_dns
