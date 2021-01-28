b903e2a6602a65509558bd804a0112a7157d90c8
Aaron D Borden <adborden@a14n.net>
2021-01-28 13:52:57 -0800

    Publish plan to pull requests (#131)
    * Move to GH action
    
    Easier for posting to PRs.
    
    * terraform fmt
    
    * Remove unused variables
    
    * Remove CircleCI

--- variables.tf
+++ variables.tf
@@ -33,16 +33,8 @@ variable "inventory_db_password" {
   description = "Master password for the inventory database server."
 }
 
-variable "inventory_2_8_db_password" {
-  description = "Master password for the inventory 2.8 database server."
-}
-
 variable "inventory_next_db_password" {
   description = "Master password for the inventory-next database server."
-}
-
-variable "inventory_2_8_redis_password" {
-  description = "Redis password for the inventory-2-8 server."
 }
 
 variable "inventory_next_redis_password" {
