f0559f77c02fa5edf7318448deba277a3ae8e180
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2021-02-25 16:21:53 -0800

    Update NS records for SSB subdomains

--- ssb.tf
+++ ssb.tf
@@ -6,9 +6,24 @@ resource "aws_route53_record" "ssb" {
   zone_id         = "Z2QMRHTV5AP7G6"
 
   records = [
-    "ns-1116.awsdns-11.org.",
-    "ns-117.awsdns-14.com.",
-    "ns-1954.awsdns-52.co.uk.",
-    "ns-766.awsdns-31.net.",
+    "ns-1147.awsdns-15.org",
+    "ns-1786.awsdns-31.co.uk",
+    "ns-28.awsdns-03.com",
+    "ns-658.awsdns-18.net",
+  ]
+}
+
+resource "aws_route53_record" "ssb_staging" {
+  allow_overwrite = true
+  name            = "ssb-staging"
+  ttl             = 30
+  type            = "NS"
+  zone_id         = "Z2QMRHTV5AP7G6"
+
+  records = [
+    "ns-1148.awsdns-15.org",
+    "ns-1937.awsdns-50.co.uk",
+    "ns-377.awsdns-47.com",
+    "ns-965.awsdns-56.net",
   ]
 }
