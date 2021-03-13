6ed6e3341bfaa50bd1766c85d3da7e9eb8eb1338
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2021-03-12 18:30:51 -0800

    Increase TTL to 15 minutes (#136)
    * Increase TTL to 15 minutes
    
    * Add nameservers for ssb-dev.datagov.us
    
    * Remove old extraneous workflow

--- ssb.tf
+++ ssb.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record" "ssb" {
   allow_overwrite = true
   name            = "ssb"
-  ttl             = 30
+  ttl             = 1800
   type            = "NS"
   zone_id         = "Z2QMRHTV5AP7G6"
 
@@ -16,7 +16,7 @@ resource "aws_route53_record" "ssb" {
 resource "aws_route53_record" "ssb_staging" {
   allow_overwrite = true
   name            = "ssb-staging"
-  ttl             = 30
+  ttl             = 1800
   type            = "NS"
   zone_id         = "Z2QMRHTV5AP7G6"
 
@@ -25,5 +25,20 @@ resource "aws_route53_record" "ssb_staging" {
     "ns-1937.awsdns-50.co.uk",
     "ns-377.awsdns-47.com",
     "ns-965.awsdns-56.net",
+  ]
+}
+
+resource "aws_route53_record" "ssb_dev" {
+  allow_overwrite = true
+  name            = "ssb-dev"
+  ttl             = 1800
+  type            = "NS"
+  zone_id         = "Z2QMRHTV5AP7G6"
+
+  records = [
+    "ns-504.awsdns-63.com",
+    "ns-1496.awsdns-59.org",
+    "ns-737.awsdns-28.net",
+    "ns-1674.awsdns-17.co.uk"
   ]
 }
