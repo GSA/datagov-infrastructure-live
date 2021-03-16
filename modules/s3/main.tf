ba6b4f3834d2ba0e820429d3774029c780487a2d
James Brown <james.c.brown@gsa.gov>
2021-03-16 15:36:03 -0600

    Update config

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -9,9 +9,12 @@ terraform {
 resource "aws_s3_bucket" "default" {
   bucket = var.bucket_name
   acl    = var.bucket_acl
-  BucketEncryption:
-    ServerSideEncryptionConfiguration:
-      - ServerSideEncryptionByDefault:
-          SSEAlgorithm: 'aws:kms'
-          KMSMasterKeyID: KMS-KEY-ARN
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.mykey.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
 }
